7bbf5a1287427d8aec10667c44d99b000478772b
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-04-07 16:18:04 +0700

    add api get events

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,9 +1,36 @@
 const asyncHandler = require("express-async-handler");
 const EventModel = require("../models/eventModel");
 
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+function calcDistanceLocation({
+    currentLat,
+    currentLong,
+    addressLat,
+    addressLong,
+}) {
+    const r = 6371; // km
+    const dLat = toRad(addressLat - currentLat);
+    const dLon = toRad(addressLong - currentLong);
+    const lat1 = toRad(addressLat);
+    const lat2 = toRad(currentLat);
+
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.sin(dLon / 2) *
+            Math.sin(dLon / 2) *
+            Math.cos(lat1) *
+            Math.cos(lat2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = r * c;
+    return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(value) {
+    return (value * Math.PI) / 180;
+}
 const addNewEvent = asyncHandler(async (req, res) => {
     const body = req.body;
-    console.log("body", body);
     if (body) {
         const newEvent = await EventModel(body);
         newEvent.save();
@@ -16,7 +43,31 @@ const addNewEvent = asyncHandler(async (req, res) => {
         throw new Error("Event data not found");
     }
 });
+const getEvents = asyncHandler(async (req, res) => {
+    const { lat, long, distance } = req.query;
+    // Tìm tất cả events
+    const events = await EventModel.find({});
+    const items = [];
+    if (events.length > 0) {
+        events.forEach((event) => {
+            const eventDistance = calcDistanceLocation({
+                currentLat: lat,
+                currentLong: long,
+                addressLat: event.location.lat,
+                addressLong: event.location.long,
+            });
+            if (eventDistance < distance) {
+                items.push(event);
+            }
+        });
+    }
+    res.status(200).json({
+        message: "Get events successfully",
+        data: items,
+    });
+});
 
 module.exports = {
     addNewEvent,
+    getEvents,
 };
--- src/routers/eventRouter.js
+++ src/routers/eventRouter.js
@@ -1,5 +1,6 @@
 const Router = require("express");
-const { addNewEvent } = require("../controllers/eventController");
+const { addNewEvent, getEvents } = require("../controllers/eventController");
 const eventRouter = Router();
 eventRouter.post("/add-new", addNewEvent);
+eventRouter.get("/get-events", getEvents);
 module.exports = eventRouter;
